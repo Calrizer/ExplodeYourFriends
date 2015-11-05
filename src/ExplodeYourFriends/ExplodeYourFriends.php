658cc6b28fb811cb037fa9302c7c0a8febe266f7
Calrizer <Callum@Family-iMac.home>
2015-11-05 07:26:33 +0000

    Added all documents.

---
+++ src/ExplodeYourFriends/ExplodeYourFriends.php
@@ -0,0 +1,79 @@
+<?php
+
+//This is open source, please feel free to use any of my code in your plugins!
+//Just don't re-upload it as your own work!
+//
+//By Calrizer (Callum Drain).
+
+namespace ExplodeYourFriends;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\event\entity\ExplosionPrimeEvent;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\Server;
+use pocketmine\entity\PrimedTNT;
+use pocketmine\level\Explosion;
+use pocketmine\level\Position;
+
+class ExplodeYourFriends extends PluginBase implements Listener{
+    
+    public function onEnable(){
+        
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+    
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        
+        switch($command->getName()){
+                
+                case "explode":
+				if(count($args) !== 1){
+					$sender->sendMessage(TextFormat::RED . "Usage: " . $command->getUsage());
+					return true;
+				}
+				
+                $player = $args[0];
+                
+                $this->explodePlayer($player, $sender);
+                
+                return true;
+        }
+        
+    }
+    
+    public function explodePlayer($player, $sender){
+        
+        $exploded = false;
+        
+        $force = 10;
+        
+        foreach($this->getServer()->getOnlinePlayers() as $p){
+            if($p->getName() === $player){
+                
+                $sender->sendMessage("§f[§cEYF§f] §9".$player." §fwas blown up!");
+                
+                $p->sendMessage("§c".$sender->getName()." §fblew you up!");
+                
+                $position = $p->getPosition();
+                
+                $explosion = new Explosion($position, $force, $this);
+                    
+                $explosion->explodeB();
+                
+                $exploded = true;
+                
+                break;
+                
+            }
+        }
+        
+        if($exploded === false){
+            $sender->sendMessage("§f[§cEYF§f] Could not find player §9".$player);
+        }
+    }
+}
+?>
